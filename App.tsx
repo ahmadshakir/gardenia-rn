96cc8ce67214d34223a742e3efeb1a2fb25a9bcb
ahmad shakir <ahkay13@gmail.com>
2023-10-25 15:01:14 +0800

    update

--- App.tsx
+++ App.tsx
@@ -20,6 +20,7 @@ const survey = () => {
   const [inputData, setInputData] = useState<{text: string; index: number}[]>(
     [],
   );
+  const [submittedId, setSubmittedId] = useState<{id: number}[]>([]);
 
   const addValues = (text: string, index: number) => {
     const dataArray = inputData;
@@ -52,18 +53,47 @@ const survey = () => {
     }
   };
 
-  // const postSurvey = async () => {
-  //   Alert.alert(JSON.stringify(inputData));
-  //   return;
-  //   try {
-  //     const response = await fetch('http://localhost:7240/api/SurveyQuestion');
-  //     const json = await response.json();
-  //     Alert.alert(JSON.stringify(json));
-  //     return json.movies;
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
+  const postSurvey = async () => {
+    try {
+      const response = await fetch('http://localhost:7240/api/Survey', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          submitTime: new Date(),
+        }),
+      });
+      const json = await response.json();
+      setSubmittedId(json.id);
+      postAnswer();
+      return json;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const postAnswer = async () => {
+    try {
+      const response = await fetch('http://localhost:7240/api/SurveyAnswer', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          surveysFk: 1,
+          answer: 'ok',
+        }),
+      });
+      const json = await response.json();
+      Alert.alert(JSON.stringify(json));
+      return json;
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
     getSurveyQuestion();
@@ -92,47 +122,6 @@ const survey = () => {
       </ScrollView>
     </SafeAreaView>
   );
-};
-
-const postSurvey = async () => {
-  try {
-    const response = await fetch('http://localhost:7240/api/Survey', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        submitTime: new Date(),
-      }),
-    });
-    const json = await response.json();
-    Alert.alert(JSON.stringify(json.id));
-    return json;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-const postAnswer = async () => {
-  try {
-    const response = await fetch('http://localhost:7240/api/SurveyAnswer', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        surveysFk: new Date(),
-        answer: new Date(),
-      }),
-    });
-    const json = await response.json();
-    Alert.alert(JSON.stringify(json.id));
-    return json;
-  } catch (error) {
-    console.error(error);
-  }
 };
 
 const styles = StyleSheet.create({
